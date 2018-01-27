f3c155dfb3895a775f928e3f345fff8fd2e69ad2
Om Sitapara <omsitapara23@gmail.com>
2018-01-27 15:56:40 +0530

    added new vec2 header file and cpp file

--- Sparky-core/main.cpp
+++ Sparky-core/main.cpp
@@ -1,10 +1,12 @@
 #include "src\graphics\window.h"
-
+#include "src\maths\vec2.h"
 using namespace std;
 
 int main() {
 	using namespace sparky;
 	using namespace graphics;
+	using namespace maths;
+
 
 	Window window("Sparky", 960, 540);
 	glClearColor(0.2f, 0.3f, .8f, 1.0f);
@@ -13,12 +15,12 @@ int main() {
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
+	vec2 vector(1.0f, 2.0f);
+
 	while (!window.closed()) {
 		// cout << window.getWidth() << " " << window.getHeight() << endl;
 		window.clear();
-		double x, y;
-		window.getMousePosition(x, y);
-		cout << x << ", " << y << endl;
+		cout << vector << endl;
 #if 1
 		glBegin(GL_QUADS);
 		glVertex2f(-0.5f, -0.5f);
---
+++ Sparky-core/src/maths/vec2.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+namespace sparky {
+	namespace maths {
+
+		struct vec2
+		{
+			float x, y;
+			vec2();
+			vec2(const float &x, const float& y);
+			vec2& add(const vec2& other);
+			vec2& subtract(const vec2& other);
+			vec2& multiply(const vec2& other);
+			vec2& divide(const vec2& other);
+
+			friend std::ostream& operator<<(std::ostream& stream, const vec2& vector);
+		};
+
+
+} }
